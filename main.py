6cf7c3edd44fc151a935026ec95f38a456ca03fe
Tyler Nielsen <tylernielsenn@gmail.com>
2022-03-22 00:07:31 -0600

    switched from gui to terminal app

--- main.py
+++ main.py
@@ -1,72 +1,19 @@
-from tkinter import *
-import tkinter as tk
-from tkinter import ttk
+from scrapereddit import Scraper
 
-from scrapereddit import * 
+id = input("Enter Client ID: ")
+secret = input("Enter Secret ID: ")
 
-first = Tk()
-first.geometry("500x500")
-first.title("Reddit Image Scraping")
+scraper = Scraper(id, secret)
 
-running = False
+#subreddit, numImages, min_width, min_height, savepath):
+subreddit = input("Enter subreddit: ")
+numImages = int(input("Count: "))
+min_width = int(input("Minimum width: "))
+min_height = int(input("Minimum height: "))
+savepath = input("Save Folder: ")
 
-# subreddit
-labelSub = tk.Label(first, text="Enter subreddit name: ")
-labelSub.grid(row=0,column=0)
+print("Scraping... ")
 
-subInput = tk.Entry(first)
-subInput.grid(row=0,column=1)
+scraper.scrapeReddit(subreddit, numImages, min_width, min_height, savepath)
 
-# num images
-labelImageNum = tk.Label(first, text="Number of Images to Retrieve")
-labelImageNum.grid(row=1,column=0)
-
-numInput = tk.Entry(first)
-numInput.grid(row=1, column=1)
-
-# min width
-sortLabel = tk.Label(first, text="Eliminate collected images with: ")
-labelWidth = tk.Label(first, text="Minimum Width: ")
-sortLabel.grid(row=3, column=0)
-labelWidth.grid(row=4,column=0)
-
-widthInput = tk.Entry(first)
-widthInput.grid(row=4,column=1)
-
-# min hieght
-
-labelHeight = tk.Label(first, text="Minimum Hieght:")
-labelHeight.grid(row=5,column=0)
-
-heightInput = tk.Entry(first)
-heightInput.grid(row=5,column=1)
-
-# save path
-labelpath = tk.Label(first, text="Folder to save to (path)")
-labelpath.grid(row=6, column=0)
-
-savepath = tk.Entry(first)
-savepath.grid(row=6, column=1)
-
-# def check_running():
-#     if running == True:
-#         statuslabel.config(text="Status: Fetching images...")
-#     else:
-#         statuslabel.config(text="Status: Done")
-
-def getRequestInfo():
-    subreddit = subInput.get()
-    numImages = int(numInput.get())
-    width = int(widthInput.get())
-    height = int(heightInput.get())
-    path = savepath.get()
-    scrapeReddit(subreddit, numImages, width, height, path)
-
-#input button
-btnIn = tk.Button(first, text="Scrape!", command=getRequestInfo)
-btnIn.grid(row=7, column=1)
-
-# statuslabel = Label(first, text="Status: ")
-# statuslabel.grid(row=8, column=0)
-
-first.mainloop()
+print("Done.")
--- scrapereddit.py
+++ scrapereddit.py
@@ -8,42 +8,45 @@ import os
 
 PIL.Image.MAX_IMAGE_PIXELS = 131460604
 
-reddit = praw.Reddit(client_id='', client_secret='', user_agent='WallpaperScraper')
+class Scraper:
+    
+    def __init__(self, id, secret):
+        self.reddit = praw.Reddit(client_id=id, client_secret=secret, user_agent='WallpaperScraper')
 
-def scrapeReddit(subreddit, numImages, min_width, min_height, savepath):
-    top_posts = reddit.subreddit(subreddit).top("all",limit=numImages)
+    def scrapeReddit(self, subreddit, numImages, min_width, min_height, savepath):
+        top_posts = self.reddit.subreddit(subreddit).top("all",limit=numImages)
 
-    # for post in top_posts:
-    #     print(post.title)
+        # for post in top_posts:
+        #     print(post.title)
 
-    image_posts = []
+        image_posts = []
 
-    for post in top_posts:
-        url = post.url
-        if((url.find("jpg") >= 0 or url.find("jpeg") >= 0 or url.find("png") >= 0)
-            and (url.find("redd.it") >= 0 or url.find("imgur") >= 0)):
-            image_posts.append(post)
-    if not os.path.exists(savepath):
-        os.mkdir(savepath)
+        for post in top_posts:
+            url = post.url
+            if((url.find("jpg") >= 0 or url.find("jpeg") >= 0 or url.find("png") >= 0)
+                and (url.find("redd.it") >= 0 or url.find("imgur") >= 0)):
+                image_posts.append(post)
+        if not os.path.exists(savepath):
+            os.mkdir(savepath)
 
-    for post in image_posts:
-        print(post.url)
-        img_data = requests.get(post.url).content
-        filetype = post.url[post.url.rfind("."):]
-        filename = post.title.replace(" ","").replace(".", "").replace("\'", "").replace("\"", "").replace("\\", "").replace("/", "") + filetype
-        with open(f"{savepath}/{filename}", "wb") as handler:
-            handler.write(img_data)
+        for post in image_posts:
+            print(post.url)
+            img_data = requests.get(post.url).content
+            filetype = post.url[post.url.rfind("."):]
+            filename = post.title.replace(" ","").replace(".", "").replace("\'", "").replace("\"", "").replace("\\", "").replace("/", "") + filetype
+            with open(f"{savepath}/{filename}", "wb") as handler:
+                handler.write(img_data)
 
-    files = os.listdir(savepath)
+        files = os.listdir(savepath)
 
-    for f in files:
-        img = Image.open(f"{savepath}/{f}")
-        width = img.width
-        height = img.height
+        for f in files:
+            img = Image.open(f"{savepath}/{f}")
+            width = img.width
+            height = img.height
 
-        if(width < min_width and height < min_height):
-            os.remove(f"{savepath}/{f}")
-            files.remove(f)
+            if(width < min_width and height < min_height):
+                os.remove(f"{savepath}/{f}")
+                files.remove(f)
 
 
 
